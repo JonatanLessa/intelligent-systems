f68502754194564ce91d40e35e4073d7371b93df
JonatanLessa <jls15@aluno.ifal.edu.br>
2022-06-09 13:15:23 -0300

    Implementa o prima indians diabetes

---
+++ aprendizagem_de_maquina/redes_neurais/pima_indians_diabetes.py
@@ -0,0 +1,37 @@
+# Loading data from CSV
+from numpy import loadtxt
+from keras.models import Sequential
+from keras.layers import Dense
+
+DATA = 'D:/Dropbox/IFAL-SI/8_Periodo/Sistemas Inteligentes/intelligent-systems/aprendizagem_de_maquina/redes_neurais/pima-indians-diabetes.data.csv'
+
+# load the dataset
+dataset = loadtxt(DATA, delimiter=',')
+# split into input (X) and output (y) variables
+X = dataset[:,0:8]
+y = dataset[:,8]
+
+print(len(X))
+
+# define the keras model
+model = Sequential()
+model.add(Dense(12, input_dim=8, activation='relu'))
+model.add(Dense(8, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+# compile the keras model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+...
+# fit the keras model on the dataset
+model.fit(X, y, epochs=150, batch_size=10)
+
+# evaluate the keras model
+_, accuracy = model.evaluate(X, y)
+print('Accuracy: %.2f' % (accuracy*100))
+
+# make class predictions with the model
+predictions = (model.predict(X) > 0.5).astype(int)
+# summarize the first 5 cases
+for i in range(50):
+	print('%s => %d (expected %d)' % (X[i].tolist(), predictions[i], y[i]))
